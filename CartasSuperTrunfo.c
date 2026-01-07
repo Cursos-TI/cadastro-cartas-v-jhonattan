f0ce7e4c88fafcca31943c5ae42a41c1efc41fb9
Jhonattan Azevedo <123788903+v-jhonattan@users.noreply.github.com>
2026-01-07 19:10:26 +0000

    pausa1
    Added variable declarations and data entry for two city cards.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,41 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  // Declaraçao das variáveis da primeira carta.
+  char estado1[50];
+  char codigo_carta1[10];
+  char cidade1[50];
+  int populacao1;
+  float areaKm1;
+  float pib1;
+  int pontos_turisticos1;
 
-  // Área para entrada de dados
+  // Declaraçao das variáveis da segunda carta.
+  char estado2[50];
+  char codigo_carta2[10];
+  char cidade2[50];
+  int populacao2;
+  float areaKm2;
+  float pib2;
+  int pontos_turisticos2;
+  
+  // Entrada de dados da primeira carta.
+  printf("Cadastro da carta 1/n");
+  printf("===================/n');
+
+  printf("Digite o Estado 1: ");
+  scanf(" %[^\n]", estado1);
+
+  printf("Digite o Código da Carta 1: ");
+  scanf("%s", codigo_carta1);
+
+  printf("Digite a Cidade 1: ");
+  scanf(" %[^\n]", cidade1);
+
+  printf("Digite a População: ");
+  
+  
+  
 
   // Área para exibição dos dados da cidade
 
