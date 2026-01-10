76f13ea46737ffe6dac47905cbbb03d58e7d19b3
Jhonattan Azevedo <123788903+v-jhonattan@users.noreply.github.com>
2026-01-10 18:36:27 +0000

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -27,21 +27,60 @@ int main() {
   printf("Cadastro da carta 1/n");
   printf("===================/n');
 
-  printf("Digite o Estado 1: ");
+  printf("Digite o Estado: ");
   scanf(" %[^\n]", estado1);
 
   printf("Digite o Código da Carta 1: ");
   scanf("%s", codigo_carta1);
 
-  printf("Digite a Cidade 1: ");
+  printf("Digite a Cidade: ");
   scanf(" %[^\n]", cidade1);
 
   printf("Digite a População: ");
-  
-  
-  
+  scanf("%d", &popuplacao1);
 
-  // Área para exibição dos dados da cidade
+  printf("Digite a Área em Km: ");
+  scanf("%f", &areaKm1);
+
+  printf("Digite o PIB: ");
+  scanf("%f", &pib1);
+  
+  printf("Digite o Número de pontos turísticos: ");
+  scanf("%d", &pontos_turisticos1);
+  
+   // Entrada de dados da segunda carta.
+  printf("Cadastro da carta 2/n");
+  printf("===================/n');
+
+  printf("Digite o Estado: ");
+  scanf(" %[^\n]", estado2);
+
+  printf("Digite o Código da Carta 2: ");
+  scanf("%s", codigo_carta2);
+
+  printf("Digite a Cidade: ");
+  scanf(" %[^\n]", cidade2);
+
+  printf("Digite a População: ");
+  scanf("%d", &popuplacao2);
+
+  printf("Digite a Área em Km: ");
+  scanf("%f", &areaKm2);
+
+  printf("Digite o PIB: ");
+  scanf("%f", &pib2);
+  
+  printf("Digite o Número de pontos turísticos: ");
+  scanf("%d", &pontos_turisticos2);
+
+  // Exibição das cartas cadastradas
+  printf("\n\n ==== Cartas Cadastradas ==== \n\n");
+
+  // Exibição da primeira carta
+  printf("Carta 1: \n");
+  printf("-------- \n");
+  printf("Estado: %s", estado1);
+  
 
 return 0;
 } 
